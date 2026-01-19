ae6d235b1f00ddc1ff77a02b80137d87a5c0608f
g53032kg <krisha.gabda@student.manchester.ac.uk>
2026-01-19 13:59:31 +0000

    Finshed the frontend display for the AI Assistant Feature.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MainPage from './pages/mainPage';
 import ViewCart from './pages/viewCartPage';
 import RankingsPage from './pages/globalRankingsPage';
+import AIAssistant from './pages/AskAIPage';
 
 export default function App() {
   return(
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/' element={<MainPage />} />
         <Route path='/ViewCart' element={<ViewCart />} />
         <Route path='/Rankings' element={<RankingsPage />} />
+        <Route path='/AIAssistant' element={<AIAssistant />} />
       </Routes>
     </Router>
   )
